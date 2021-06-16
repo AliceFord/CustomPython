8e0fdda50fe8453117611b48c6ec57881d59b912
TechieHelper <oliverlford@icloud.com>
2021-06-16 14:07:16 +0100

    Code cleanup

--- lexer.py
+++ lexer.py
@@ -28,11 +28,12 @@ class CustomPythonLexer:
 		#return "<main><ins><fc><name>PRINT</name><params><_1><num>1</num><op>*</op><num>1</num></_1></params></fc></ins><ins><fc><name>PRINT</name><params><_1><num>1</num><op>+</op><num>1</num></_1></params></fc></ins></main>"
 
 	def decodeInstruction(self, ins, parentElement):
-		self.isNum = False
-		self.isOp = False
-		self.isStr = False
-		self.isVDef = False
-		startElem = -1
+		self.is_num = False
+		self.is_op = False
+		self.is_str = False
+		self.is_vdef = False
+		self.flags = ["num", "op", "str"]
+		self.startElem = -1
 		funcChecker = re.compile("([a-zA-Z][a-zA-Z0-9]*?)\((.*)\)")
 
 		isFunc = funcChecker.match(ins)
@@ -46,103 +47,53 @@ class CustomPythonLexer:
 				self.decodeInstruction(param, currentParamTag)
 		else:
 			for i, c in enumerate(ins):
-				if not self.isStr:
+				if not self.is_str:
 					try:
 						int(c)
-						self.setAllFlagsExcept("num", parentElement, ins, startElem, i)
-						if not self.isNum:
-							self.isNum = True
-							startElem = i
+						self.foundElement("num", parentElement, ins, self.startElem, i)
 					except ValueError:
-						self.setFlag("num", parentElement, ins, startElem, i)
+						self.setFlag("num", parentElement, ins, self.startElem, i)
 
 					if c in ["+", "-", "*", "/"]:
-						self.setAllFlagsExcept("op", parentElement, ins, startElem, i)
-						if not self.isOp:
-							self.isOp = True
-							startElem = i
+						self.foundElement("op", parentElement, ins, self.startElem, i)
 					else:
-						self.setFlag("op", parentElement, ins, startElem, i)
+						self.setFlag("op", parentElement, ins, self.startElem, i)
 
 				if c == "\"":
-					self.setAllFlagsExcept("str", parentElement, ins, startElem, i)
-					if not self.isStr:
-						self.isStr = True
-						startElem = i
+					self.foundElement("str", parentElement, ins, self.startElem, i)
 				else:
 					pass
 
 				if c == " ":
-					self.setAllFlagsExcept(None, parentElement, ins, startElem, i)
+					self.setAllFlagsExcept(None, parentElement, ins, self.startElem, i)
 
-			self.setAllFlagsExcept(None, parentElement, ins, startElem)
+			self.setAllFlagsExcept(None, parentElement, ins, self.startElem)
+
+	def foundElement(self, flag, parent, ins, startRange, endRange):
+		self.setAllFlagsExcept(flag, parent, ins, startRange, endRange)
+		if not getattr(self, "is_" + flag):
+			setattr(self, "is_" + flag, True)
+			self.startElem = endRange
 
 	def setFlag(self, flag, element, ins, startRange, endRange):
-		if flag == "op":
-			if self.isOp:
-				opTag = ET.SubElement(element, "op")
-				opTag.text = ins[startRange:endRange]
-			self.isOp = False
-		elif flag == "num":
-			if self.isNum:
-				numTag = ET.SubElement(element, "num")
-				numTag.text = ins[startRange:endRange]
-			self.isNum = False
-		elif flag == "str":
-			if self.isStr:
-				strTag = ET.SubElement(element, "str")
-				strTag.text = ins[startRange:endRange]
-			self.isStr = False
-		elif flag == "vdef":
-			if self.isVDef:
-				strTag = ET.SubElement(element, "vdef")
-				strTag.text = ins[startRange:endRange]
-			self.isStr = False
+		if getattr(self, "is_" + flag):
+			tag = ET.SubElement(element, flag)
+			tag.text = ins[startRange:endRange]
+		setattr(self, "is_" + flag, False)
 
 	def setAllFlagsExcept(self, flag, element, ins, startRange, endRange=None):
 		if endRange is None:
 			endRange = len(ins)
 
 		if flag is None:
-			if self.isOp:
-				opTag = ET.SubElement(element, "op")
-				opTag.text = ins[startRange:endRange]
-
-			if self.isNum:
-				numTag = ET.SubElement(element, "num")
-				numTag.text = ins[startRange:endRange]
-
-			if self.isStr:
-				strTag = ET.SubElement(element, "str")
-				strTag.text = ins[startRange:endRange]
-		elif flag == "num":
-			if self.isOp:
-				opTag = ET.SubElement(element, "op")
-				opTag.text = ins[startRange:endRange]
-			self.isOp = False
-
-			if self.isStr:
-				strTag = ET.SubElement(element, "str")
-				strTag.text = ins[startRange:endRange]
-			self.isStr = False
-		elif flag == "op":
-			if self.isNum:
-				numTag = ET.SubElement(element, "num")
-				numTag.text = ins[startRange:endRange]
-			self.isNum = False
-
-			if self.isStr:
-				strTag = ET.SubElement(element, "str")
-				strTag.text = ins[startRange:endRange]
-			self.isStr = False
-
-		elif flag == "str":
-			if self.isNum:
-				numTag = ET.SubElement(element, "num")
-				numTag.text = ins[startRange:endRange]
-			self.isNum = False
-
-			if self.isOp:
-				opTag = ET.SubElement(element, "op")
-				opTag.text = ins[startRange:endRange]
-			self.isOp = False
+			for overallFlag in self.flags:
+				if getattr(self, "is_" + overallFlag):
+					tag = ET.SubElement(element, overallFlag)
+					tag.text = ins[startRange:endRange]
+		else:
+			for overallFlag in self.flags:
+				if flag != overallFlag:
+					if getattr(self, "is_" + overallFlag):
+						tag = ET.SubElement(element, overallFlag)
+						tag.text = ins[startRange:endRange]
+					setattr(self, "is_" + overallFlag, False)
